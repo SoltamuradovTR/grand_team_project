1ed284f16190394106b221154f525eaed8c374ce
SoltamuradovTR <t.r.soltamuradov@mail.ru>
2021-08-04 13:43:34 +0300

    сделано: модель, контроллеры, роуты у заявки

---
+++ routes/requests.route.js
@@ -0,0 +1,11 @@
+const {Router} = require('express')
+const { requestsController } = require('../controllers/requests.controller')
+
+const router = Router()
+
+
+router.get('/requests', requestsController.getRequests)
+router.get('client/:id/requests', requestsController.getRequestsByClient)
+router.post('/client/:id/request', requestsController.createRequest)
+router.patch('/client/:id/request/:id', requestsController.editRequest)
+router.post('/appraisers/:id', requestsController.addAppraisers)
