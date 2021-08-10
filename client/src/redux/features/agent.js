19e24896c8e5293580609f9c5f2e27852cfa4c33
deni <deni.akhmatukaev@gmail.com>
2021-08-10 21:00:08 +0300

    доделать изменение, перенести в редьюсер login

--- client/src/redux/features/agent.js
+++ client/src/redux/features/agent.js
@@ -2,27 +2,57 @@ const initialState = {
   signingUp: false,
   signingIn: false,
   error: null,
+  editingAgent: null,
+  loading: false
 };
 
 const agent = (state = initialState, action) => {
   switch (action.type) {
-    case "agent/signUp/pending":
+    case "close/dialog":
       return {
         ...state,
-        signingUp: true,
+        editingAgent: null,
       };
-    case "agent/signUp/fulfilled":
+    case 'set/field':
+      const e = action.payload;
       return {
         ...state,
-        signingUp: false,
-      };
-    case "agent/signUp/rejected":
+        editingAgent: {
+          ...state.editingAgent,
+          [e.target.name]: e.target.value
+        }
+      }
+    case "agent/set-editing-agent":
       return {
         ...state,
-        signingUp: false,
+        editingAgent: action.payload
+      }
+    case "agent/edit/pending":
+      return {
+        ...state,
+        loading: true,
       };
-
-
+    case "agent/edit/fulfilled":         //доделать изменение локалсторейдж
+      return {
+        ...state,
+        loading: false,
+        editingAgent: null,
+        items: state.items.map((school) => {
+          if (school._id === state.editingAgent._id) {
+            return {
+              ...school,
+              ...state.editingAgent,
+            };
+          }
+          return school;
+        }),
+      };
+    case "agent/edit/rejected":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
     default:
       return state;
   }
@@ -30,5 +60,45 @@ const agent = (state = initialState, action) => {
 
 export default agent;
 
+export const closeEditingDialog = () => {
+  return {
+    type: "close/dialog",
+  };
+};
 
+export const setFormFields = (e) => {
+  return {
+    type: "set/field",
+    payload: e,
+  };
+};
 
+export const setEditingAgent = (agent) => {
+  return {
+    type: "agent/set-editing-agent",
+    payload: agent,
+  };
+};
+
+export const editAgent = () => {
+  return async (dispatch, getState) => {
+    dispatch({ type: "agent/edit/pending" });
+
+    const { agent  } = getState();
+
+    try {
+      await fetch(`/agent/${agent.editingAgent._id}`, {
+        method: "PATCH",
+        body: JSON.stringify(agent.editingAgent),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      dispatch({ type: "agent/edit/fulfilled" });
+    } catch (e) {
+      dispatch({ type: "agent/edit/rejected", error: e.toString() });
+    }
+  };
+};
+
+export const selectEditingAgent = (state) => state.agent.editingAgent;
