494f16bed36aaa663c80ff4186a8fcdec0f4832f
SoltamuradovTR <t.r.soltamuradov@mail.ru>
2021-08-15 14:26:56 +0300

    Сделан fileUpload на фронтенде

--- client/src/redux/features/agent.js
+++ client/src/redux/features/agent.js
@@ -1,27 +1,48 @@
 const initialState = {
   error: null,
   loading: false,
-  itemsById: []
+  currentItem: [],
+  itemsById: [],
 };
 
 const agent = (state = initialState, action) => {
   switch (action.type) {
-    case 'agent/getById/pending':
+    case "agent/getById/pending":
       return {
         ...state,
-        loading: true
+        loading: true,
       };
-    case 'agent/getById/fulfilled':
+    case "agent/getById/fulfilled":
       return {
         ...state,
         loading: false,
-        itemsById: [action.payload]
+        itemsById: [action.payload],
       };
-    case 'agent/getById/rejected':
+    case "agent/getById/rejected":
       return {
         ...state,
-        loading: false
-      }
+        loading: false,
+      };
+    case "avatar/add/pending":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "avatar/add/fulfilled":
+      return {
+        ...state,
+        loading: false,
+        currentItem: {
+          ...state.currentItem,
+          avatar: action.payload,
+        },
+      };
+    case "avatar/add/rejected":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
     default:
       return state;
   }
@@ -29,38 +50,60 @@ const agent = (state = initialState, action) => {
 
 export const addClientsToAgent = (clientId, agentId) => {
   return async (dispatch) => {
-    dispatch({ type: 'agent/addClient/pending'});
+    dispatch({ type: "agent/addClient/pending" });
 
     try {
       await fetch(`/add/client/to/${agentId}`, {
         method: "POST",
         body: JSON.stringify({ client: clientId }),
         headers: {
-          "Content-type": "application/json"
-        }
-      })
-      dispatch({ type: 'agent/addClient/fulfilled'})
+          "Content-type": "application/json",
+        },
+      });
+      dispatch({ type: "agent/addClient/fulfilled" });
     } catch (e) {
-      dispatch({ type: "agent/addClient/rejected", error: e.toString() })
+      dispatch({ type: "agent/addClient/rejected", error: e.toString() });
     }
-  }
-}
+  };
+};
 
 export const loadAgentById = (agentId) => {
   return async (dispatch) => {
-    dispatch({ type: 'agent/getById/pending'})
+    dispatch({ type: "agent/getById/pending" });
 
     try {
-      const res = await fetch(`/agent/${agentId}`)
+      const res = await fetch(`/agent/${agentId}`);
       const json = await res.json();
 
-      dispatch({ type: 'agent/getById/fulfilled', payload: json})
+      dispatch({ type: "agent/getById/fulfilled", payload: json });
     } catch (e) {
-      dispatch({ type: 'agent/getById/rejected', error: e.toString()})
+      dispatch({ type: "agent/getById/rejected", error: e.toString() });
     }
-  }
-}
+  };
+};
 
 export default agent;
 
-export const selectAgentById = (state) => state.agent.itemsById
+export const uploadAvatar = (file) => {
+  return async (dispatch, getState) => {
+    const state = getState();
+
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await fetch("/avatar", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${state.login.token}`,
+        },
+        body: formData,
+      });
+      const json = await res.json();
+      dispatch({ type: "avatar/add/fulfilled", payload: json.avatar });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+};
+
+export const selectAgentById = (state) => state.agent.itemsById;
