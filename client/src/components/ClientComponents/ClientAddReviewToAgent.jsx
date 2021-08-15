7243eec25415ad722bb1fb305da5ba6796e2b90e
deni <deni.akhmatukaev@gmail.com>
2021-08-15 21:48:25 +0300

    сделано добавление отзыва с редьюсером, без populate автора, не проверена возможность добавления не клиенту агента

---
+++ client/src/components/ClientComponents/ClientAddReviewToAgent.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import { selectCandidate } from "../../redux/features/login";
+import { addReview } from "../../redux/features/review";
+
+function ClientAddReviewToAgent({ agentId }) {
+  const dispatch = useDispatch();
+
+  const candidate = useSelector(selectCandidate);
+
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState(candidate._id);
+
+  const handleAddText = (e) => {
+    setText(e.target.value)}
+
+    const handleClickAddReview = () => {
+      dispatch(addReview({ text, author }, agentId));
+    };
+    return (
+      <>
+        <Accordion style={{ width: 900 }}>
+          <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
+            <Typography style={{ margin: "auto" }}>Добавить отзыв</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box>
+              <Typography>Добавить отзыв</Typography>
+              <TextField
+                style={{
+                  width: 860,
+                  height: 100,
+                }}
+                value={text}
+                onChange={handleAddText}
+              />
+              <Button
+                variant="contained"
+                style={{
+                  width: 860,
+                  color: "green",
+                }}
+                onClick={handleClickAddReview}
+              >
+                Добавить отзыв
+              </Button>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+      </>
+    );
+}
+
+export default ClientAddReviewToAgent;
