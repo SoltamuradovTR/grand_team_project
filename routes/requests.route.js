8962f3ba4949b05651b603288ebcad5dfb077070
SoltamuradovTR <t.r.soltamuradov@mail.ru>
2021-08-15 17:58:33 +0300

    приступили к завершению записей

--- routes/requests.route.js
+++ routes/requests.route.js
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/requests", requestsController.getRequests);
 router.get("/request/:id", requestsController.getRequestsByClient);
 router.post("/client/:id/request", requestsController.createRequest);
-router.patch("/client/:id/request/:id", requestsController.editRequest);
+router.patch("/request/:id", requestsController.editRequest);
 router.post("/appraisers/:id", requestsController.addAppraisers);
 router.delete("/request/:id", requestsController.deleteRequest);
 
